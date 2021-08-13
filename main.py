05c02b4b7bc6634b9b77b12a13235817c518d00e
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-08-13 05:25:12 -0300

    Finalizando calculo

--- main.py
+++ main.py
@@ -7,28 +7,20 @@ api = Api(app)
 
 class Sumtest(Resource):
     def post(self):
-        dados = json.loads(request.data)
-        lista = dados['list']
-        sequencia = ""
-        parcial = 0
+        lista = json.loads(request.data)['list']
+        result = 0
         size = len(lista)
-        contador = 0
         for i in range(size):
-            atual = 0
+            current = 0
             for j in range(i,size):
-                contador+=1
-                atual += lista[j]
-                if atual > parcial:
-                    parcial = atual
-                    sequencia = i #"{0}:{1}".format(i,j)
+                current += lista[j]
+                if current > result:
+                    result = current
         return {
-            "data": lista,
-            "result": parcial,
-            "contador":contador,
-            "best-sequence": sequencia
+            "List": lista,
+            "result": result
         }
 
-
-api.add_resource(Sumtest, '/')
+api.add_resource(Sumtest, '/maxsum/')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
